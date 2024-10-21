e1900aab1ff20889f438b1a10a784c43a663fde9
hekai000 <675974119@qq.com>
2024-10-21 15:22:31 +0800

    hk commit feature)add leetcode 1520 solution

---
+++ LeetCode100/maxDistance.go
@@ -0,0 +1,36 @@
+package leetcode100
+
+import "sort"
+
+func maxDistance(position []int, m int) int {
+	if m == 0 {
+		return 0
+	}
+	slice := position[:]
+	sort.Ints(slice)
+	left, right, ans := 1, position[len(position)-1]-position[0], 0
+	for left <= right {
+		mid := (left + right) / 2
+		if check(slice, mid, m) {
+			ans = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return ans
+}
+func check(position []int, mid int, m int) bool {
+	count := 1
+	last := position[0]
+	for i := 1; i < len(position); i++ {
+		if position[i]-last >= mid {
+			count++
+			last = position[i]
+			if count >= m {
+				return true
+			}
+		}
+	}
+	return false
+}
---
+++ LeetCode100/maxDistance_test.go
@@ -0,0 +1,29 @@
+package leetcode100
+
+import "testing"
+
+func TestMaxDistance(t *testing.T) {
+	tests := []struct {
+		position []int
+		m        int
+		expected int
+	}{
+		// Happy path
+		{[]int{1, 2, 3, 4, 7}, 3, 3},
+		{[]int{1, 3, 6, 7, 10}, 3, 4},
+
+		// Edge cases
+		{[]int{1, 2}, 2, 1},                  // Minimum distance for 2 positions
+		{[]int{1, 1000000000}, 2, 999999999}, // Maximum distance in large numbers
+		{[]int{1}, 1, 0},                     // Only one position
+		// {[]int{}, 1, 0},                      // No positions
+		{[]int{1, 2, 3, 4, 5}, 0, 0}, // m = 0
+	}
+
+	for _, test := range tests {
+		result := maxDistance(test.position, test.m)
+		if result != test.expected {
+			t.Errorf("For position %v and m = %d, expected %d but got %d", test.position, test.m, test.expected, result)
+		}
+	}
+}
