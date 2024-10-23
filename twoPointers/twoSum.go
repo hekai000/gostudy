dce6d2d3ec1334e1efcc5b72c38a31cbca38bb2a
hekai000 <675974119@qq.com>
2024-10-23 17:45:38 +0800

    hk commit feature)add twoSum的双指针解法

---
+++ twoPointers/twoSum.go
@@ -0,0 +1,36 @@
+package twopointers
+
+import "sort"
+
+// 使用双指针解法解twoSum问题
+func twoSum(nums []int, target int) []int {
+	if len(nums) <= 1 {
+		return []int{}
+	}
+	nums2 := make([]int, len(nums))
+	copy(nums2, nums)
+
+	sort.Ints(nums2)
+	left, right := 0, len(nums2)-1
+	res := make([]int, 2)
+
+	for left < right && nums2[left]+nums2[right] != target {
+		if nums2[left]+nums2[right] > target {
+			right--
+		} else {
+			left++
+		}
+	}
+	for index1, num := range nums {
+		if num == nums2[left] {
+			res[0] = index1
+		}
+	}
+	for index2, num := range nums {
+		if num == nums2[right] && index2 != res[0] {
+			res[1] = index2
+			break
+		}
+	}
+	return res
+}
---
+++ twoPointers/twoSum_test.go
@@ -0,0 +1,30 @@
+package twopointers
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		nums   []int
+		target int
+		result []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},        // Happy path
+		{[]int{3, 2, 4}, 6, []int{1, 2}},             // Happy path
+		{[]int{3, 3}, 6, []int{1, 0}},                // Happy path
+		{[]int{1, 5, 7, 3, 2, 4}, 10, []int{3, 2}},   // Edge case with larger array
+		{[]int{0, 2, 4, 3, 3}, 6, []int{1, 2}},       // Edge case with duplicates
+		{[]int{-1, -2, -3, -4, -5}, -8, []int{4, 2}}, // Edge case with negative numbers
+		{[]int{}, 0, []int{}},                        // Edge case with empty array
+	}
+
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := twoSum(test.nums, test.target); !reflect.DeepEqual(got, test.result) {
+				t.Errorf("twoSum(%v, %d) = %v; want %v", test.nums, test.target, got, test.result)
+			}
+		})
+	}
+}
